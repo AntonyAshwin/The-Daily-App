d56e1a01a243da2e197bafba4c40b1f25a3042b2
Antony Ashwin <antonyashwin555@gmail.com>
2026-03-25 23:11:44 +0530

    Initial Commit

---
+++ Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by Ashwin, Antony on 25/03/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
